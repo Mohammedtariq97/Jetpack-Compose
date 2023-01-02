f84d37656b5f0faf44ef6163d4aae1b048b243ff
Mohammedtariq97 <ms.tariq97@gmail.com>
2023-01-02 13:09:39 +0530

    Learnt state hoisting

--- ComposeTutorial/app/src/main/java/com/emproto/composetutorial/ComposeBasicActivity.kt
+++ ComposeTutorial/app/src/main/java/com/emproto/composetutorial/ComposeBasicActivity.kt
@@ -4,13 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.ElevatedButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -28,7 +26,40 @@ class ComposeBasicActivity : ComponentActivity() {
 }
 
 @Composable
-private fun MyApp(modifier: Modifier = Modifier, names: List<String> = listOf( "Tom","Jerry")) {
+private fun MyApp(modifier: Modifier = Modifier) {
+    val shouldShowOnBoarding = remember {
+        mutableStateOf(true)
+    }
+    Surface(modifier) {
+        if(shouldShowOnBoarding.value){
+            OnBoardingScreen(onContinueClicked = { shouldShowOnBoarding.value = false})
+        }else{
+            Greetings()
+        }
+    }
+}
+
+@Composable
+fun OnBoardingScreen( onContinueClicked : () -> Unit,
+                      modifier: Modifier = Modifier){
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Welcome to Compose!")
+        Button(modifier = Modifier.padding(vertical = 24.dp),
+            onClick = onContinueClicked ) {
+            Text(text = "Continue")
+        }
+    }
+}
+
+@Composable
+private fun Greetings(
+    modifier: Modifier = Modifier,
+    names: List<String> = listOf( "Tom","Jerry")
+){
     Column(modifier = modifier.padding(vertical = 4.dp)) {
         for(name in names){
             Greeting(name = name)
@@ -47,7 +78,9 @@ fun Greeting(name: String) {
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier.weight(1f).padding(bottom = extraPadding)) {
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(bottom = extraPadding)) {
                 Text(text = "Hello,")
                 Text(text = name)
             }
@@ -61,11 +94,28 @@ fun Greeting(name: String) {
     }
 }
 
-@Preview(showBackground = true,widthDp = 320)
+@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun MyAppPreview() {
     ComposeTutorialTheme {
-        MyApp()
+        MyApp(Modifier.fillMaxSize())
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+private fun GreetingsPreview(){
+    ComposeTutorialTheme {
+        Greetings()
+    }
+}
+
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun onBoardingPreview(){
+    ComposeTutorialTheme {
+        OnBoardingScreen(onContinueClicked = { })
     }
 }
 
