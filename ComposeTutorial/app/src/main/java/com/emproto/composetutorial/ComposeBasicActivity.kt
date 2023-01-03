267cf55d17f5d3a274e28e55e6e32f783fad2da9
Mohammedtariq97 <ms.tariq97@gmail.com>
2023-01-03 18:35:32 +0530

    List and animations added

--- ComposeTutorial/app/src/main/java/com/emproto/composetutorial/ComposeBasicActivity.kt
+++ ComposeTutorial/app/src/main/java/com/emproto/composetutorial/ComposeBasicActivity.kt
@@ -3,11 +3,18 @@ package com.emproto.composetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -27,7 +34,7 @@ class ComposeBasicActivity : ComponentActivity() {
 
 @Composable
 private fun MyApp(modifier: Modifier = Modifier) {
-    val shouldShowOnBoarding = remember {
+    val shouldShowOnBoarding = rememberSaveable {
         mutableStateOf(true)
     }
     Surface(modifier) {
@@ -40,8 +47,7 @@ private fun MyApp(modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun OnBoardingScreen( onContinueClicked : () -> Unit,
-                      modifier: Modifier = Modifier){
+fun OnBoardingScreen( onContinueClicked : () -> Unit, modifier: Modifier = Modifier){
     Column(
         modifier = modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
@@ -58,10 +64,10 @@ fun OnBoardingScreen( onContinueClicked : () -> Unit,
 @Composable
 private fun Greetings(
     modifier: Modifier = Modifier,
-    names: List<String> = listOf( "Tom","Jerry")
+    names: List<String> = List(1000) { "$it" }
 ){
-    Column(modifier = modifier.padding(vertical = 4.dp)) {
-        for(name in names){
+    LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+        items(items = names) {name ->
             Greeting(name = name)
         }
     }
@@ -72,7 +78,12 @@ fun Greeting(name: String) {
     val expanded = remember {
         mutableStateOf(false)
     }
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    val extraPadding by animateDpAsState(targetValue = if (expanded.value) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
@@ -80,7 +91,7 @@ fun Greeting(name: String) {
         Row(modifier = Modifier.padding(24.dp)) {
             Column(modifier = Modifier
                 .weight(1f)
-                .padding(bottom = extraPadding)) {
+                .padding(bottom = extraPadding.coerceAtLeast(0.dp))) {
                 Text(text = "Hello,")
                 Text(text = name)
             }
@@ -118,4 +129,6 @@ fun onBoardingPreview(){
         OnBoardingScreen(onContinueClicked = { })
     }
 }
+
+
 
