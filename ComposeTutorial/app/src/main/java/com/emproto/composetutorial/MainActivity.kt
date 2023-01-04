e9ef1966aee3332c7a5ebc73a0b667369e70774a
Mohammedtariq97 <ms.tariq97@gmail.com>
2023-01-04 17:28:42 +0530

    Card addition and theming styling

--- ComposeTutorial/app/src/main/java/com/emproto/composetutorial/MainActivity.kt
+++ ComposeTutorial/app/src/main/java/com/emproto/composetutorial/MainActivity.kt
@@ -16,14 +16,18 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.emproto.composetutorial.ui.theme.ComposeTutorialTheme
+import com.emproto.composetutorial.ui.theme.*
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
